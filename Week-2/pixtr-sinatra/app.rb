0f1963930d6711a7997aa2d81c74a34974052336
Sammyfrw <sammyfrw@gmail.com>
2014-06-11 14:10:35 -0400

    Updated week 2 files for pixtr-sinatra study
    fixed dogs.erb

--- Week-2/pixtr-sinatra/app.rb
+++ Week-2/pixtr-sinatra/app.rb
@@ -1,15 +1,32 @@
 require "sinatra"
+require "pg"
+require "active_record"
+
+ActiveRecord::Base.establish_connection(
+  adapter: "postgresql",
+  database: "photo_gallery",
+  username: "postgres",
+  password: ""
+  )
+
+  class Gallery < ActiveRecord::Base
+  end
+
+get "/" do
+  @galleries = Gallery.all
+  erb :index
+end
+
 
 get "/:gallery_name" do
 cat_images_in_app_rb = ["colonel_meow.jpg", "grumpy_cat.png"]
 dog_images_in_app_rb = ["shibe.png"]
   gallery_name = params[:gallery_name] 
-  (erb gallery_name.to_sym, {locals: {
+  erb(gallery_name.to_sym, {
+  	locals: {
   	cat_images: cat_images_in_app_rb, 
-  	dog_images: dog_images_in_app_rb}
-  	})
+  	dog_images: dog_images_in_app_rb
+  }
+  })
 end
 
-##erb lets you render html file with embedded ruby - putting ruby in your
-##files. It will change your ruby in your files and mix with HTML. In this
-##case, it looks for a cats.erb and presents it to the browser.
