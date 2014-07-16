b44bc65c56b6315cda258df589f1c422e0cc743b
Sammyfrw <sammyfrw@gmail.com>
2014-07-16 13:42:58 -0400

    Moved argument to new PropertyQuery class instead of method in class

--- Week-7/hotel/prop_query.rb
+++ Week-7/hotel/prop_query.rb
@@ -1,17 +1,18 @@
-require 'csv'
+require "csv"
 require "./property"
 
 class PropertyQuery
-  def initialize
+  def initialize(filename)
+    @file = filename
   end
 
-  def read_file(filename)
-    CSV.foreach(filename, headers: :true) do |row|
+  def read_file
+    CSV.foreach(@file, headers: :true) do |row|
       property = Property.new(row)
       puts property.name
     end
   end
 end
 
-property_query = PropertyQuery.new
-property_query.read_file('./hotels.csv')
+property_query = PropertyQuery.new('./hotels.csv')
+property_query.read_file
