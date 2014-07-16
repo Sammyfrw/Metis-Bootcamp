9514145a6c1ad722501609da226a0bc2d9567c8d
Sammyfrw <sammyfrw@gmail.com>
2014-07-16 14:39:09 -0400

    Added querying feature to find hotel name

--- Week-7/hotel/prop_query.rb
+++ Week-7/hotel/prop_query.rb
@@ -4,15 +4,31 @@ require "./property"
 class PropertyQuery
   def initialize(filename)
     @file = filename
+    @property = []
+  end
+
+  def begin_query
+    read_file
+    query_for_property
   end
 
   def read_file
     CSV.foreach(@file, headers: :true) do |row|
-      property = Property.new(row)
-      puts property.name
+      @property << Property.new(row)
     end
+  end
+
+  def query_for_property
+    print "Please input the hotel name that you wish details for. > "
+    query = gets.chomp
+    query_result = @property.detect {|property| property.name == query}
+    show_result(query_result)
+  end
+
+  def show_result(query_result)
+    puts query_result.name
   end
 end
 
 property_query = PropertyQuery.new('./hotels.csv')
-property_query.read_file
+property_query.begin_query
