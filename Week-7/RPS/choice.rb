475a4ae08262b1c66ca8eb32097488a5f8e50cda
Sammyfrw <sammyfrw@gmail.com>
2014-07-17 16:56:19 -0400

    Changed method names for clarity

--- Week-7/RPS/choice.rb
+++ Week-7/RPS/choice.rb
@@ -10,18 +10,18 @@ class Choice
   def set_up_choices
     make_choice
     set_ai_choice
-    compare_choices
+    set_outcome
   end
 
   private
 
   def make_choice
-    while is_choice_valid? == false do
+    while invalid_choice?
     prompt_entry
     @player_choice = gets.chomp
     validate_choice
-    end
   end
+end
 
   def prompt_entry
     print "Enter R/P/S to make a choice, or Q to quit. > "
@@ -29,23 +29,23 @@ class Choice
 
   def validate_choice
     quit_game_if_player_wants_to!
-    if !is_choice_valid?
+    if invalid_choice?
       puts "That's not a valid choice!"
     end
   end
 
-  def is_choice_valid?
-    CHOICES.include?(@player_choice)
+  def invalid_choice?
+    !CHOICES.include?(@player_choice)
   end
 
   def quit_game_if_player_wants_to!
-    if is_player_quitting?
+    if player_is_quitting?
       puts "Ending game, see you next time!"
       exit
     end
   end 
 
-  def is_player_quitting?
+  def player_is_quitting?
     @player_choice == QUIT_CHOICE
   end
 
@@ -53,7 +53,7 @@ class Choice
     @ai_choice = CHOICES.sample
   end
 
-  def compare_choices
+  def set_outcome
     puts "Player uses #{@player_choice}, AI uses #{@ai_choice}!"
     outcome = Outcome.new(@player_choice + @ai_choice)
   end
